30e14c83b631d92d3db27e6f34bec5e507732b8e
Jorge Muñoz Aguado <jorge.munnoza@estudiante.uam.es>
2019-12-19 14:24:15 +0100

    Centrar versión 2. Seguir realizando pruebas. Y añadir validaciión cruzada

--- test.py
+++ test.py
@@ -9,36 +9,66 @@ import os
 import numpy as np
 
 
+
+def center_image(img):
+
+    height, width = img.shape
+
+    col_sum = np.where( np.sum(img, axis=0) > 0 )
+    row_sum = np.where( np.sum(img, axis=1) > 0 )
+
+    y1, y2 = row_sum[0][0], row_sum[0][-1]
+    x1, x2 = col_sum[0][0], col_sum[0][-1]
+
+    cropped_image = img[y1:y2, x1:x2]
+
+    move_y = int(((y1 + height - y2) / 2) - y1)
+    move_x = int(((x1 + width - x2) / 2) - x1)
+
+    tf_form = tf.AffineTransform(translation=(-move_x, -move_y))
+
+    return tf.warp(img, tf_form)
+
+
+
+
 def transform_image(img):
 
     images = []
+    t_form = []
 
-    # tform = tf.AffineTransform(scale=(1.3, 1.1), rotation=0.5, translation=(0, -20))
-    # tform = tf.AffineTransform(shear=0.3, translation=(30, 0))
-    # tform = tf.AffineTransform(scale=(1.3, 1.1), shear=-0.3, translation=(-50, 0))
-    # tform = tf.AffineTransform(rotation=0.5)
-    # tform = tf.AffineTransform(translation=(0, -20))
+    # t_form.append( tf.AffineTransform(scale=(1.2, 1), shear=0.2) )
+    # t_form.append( tf.AffineTransform(scale=(1.2, 1), shear=-0.2) )
 
+    # t_form.append( tf.AffineTransform(rotation=0.2) )
+    # t_form.append( tf.AffineTransform(rotation=0.2) )
 
-    # t1_form = tf.AffineTransform(scale=(1.5, 1.3), shear=0.2)
     # t2_form = tf.AffineTransform(scale=(2.4, 2.2), translation=(-60, -80))
     # t3_form = tf.AffineTransform(scale=(3, 2.8), translation=(-60, -80))
     # t4_form = tf.AffineTransform(shear=0.2, translation=(30, 0))
     # t5_form = tf.AffineTransform(scale=(1.5, 1.3), shear=-0.2, translation=(-50, 0))
     # t6_form = tf.AffineTransform(scale=(1.4, 1.2), rotation=0.2)
-    t7_form = img * np.random.randint(2, size=img.shape)
-    t7_form = img * np.random.random_sample(img.shape)
+    # t7_form = img * np.random.choice([0, 1], img.shape, p=[0.1, 0.9])
+    # t8_form = img * np.random.choice([0, 1], img.shape, p=[0.25, 0.75])
 
 
-    # images.append( img )
+    images.append( img )
 
-    # images.append( tf.warp(img, t1_form) )
-    # images.append( tf.warp(img, t2_form) )
-    # images.append( tf.warp(img, t3_form) )
-    # images.append( tf.warp(img, t4_form) )
-    # images.append( tf.warp(img, t5_form) )
-    # images.append( tf.warp(img, t6_form) )
-    images.append( t7_form )
+
+    for n_form in t_form:
+
+        img_tf = tf.warp(img, n_form)
+
+        img_tf[(img_tf > 0.6)] = 1
+        img_tf[(img_tf <= 0.6)] = 0
+
+        img_tf = center_image( img_tf )
+
+        images.append( img_tf )    
+
+
+    # images.append( t7_form )
+    # images.append( t8_form )
     
 
     # return [tf.warp(img, tform)]
@@ -70,26 +100,18 @@ dir_img = files[random.randint(0, len(files) - 1)]
 print(dir_img)
 
 # dir_img = "00421_e.png"
+# dir_img = "00032_i.png"
+# dir_img = "00602_i.png"
 
 
 img1 = plt.imread(dirname + dir_img)
 
-# img1 = transform_image(img1)
-# img1_t = median(img1, )
-# img1_t = median(img1_t)
-
-
 img1_t = delete_background(img1)
 
-# img1_t[(img1_t == 0)] = 2
-# img1_t[(img1_t == 1)] = 0
-# img1_t[(img1_t == 2)] = 1
+img1_t = center_image(img1_t)
 
-# img1_t = transform_image(img1_t)
 images = transform_image(img1_t)
 
-print(img1.shape)
-print(img1_t.shape)
 
 for transf in images:
 
@@ -98,6 +120,7 @@ for transf in images:
     ax1 = fig.add_subplot(121)  # left side
     ax2 = fig.add_subplot(122)  # right side
 
+    # ax1.imshow(img1_t, cmap='gray')
     ax1.imshow(img1, cmap='gray')
     ax2.imshow(transf, cmap='gray')
 
--- utils.py
+++ utils.py
@@ -22,11 +22,11 @@ def transform_image(img):
     images = []
     t_form = []
 
-    t_form.append( tf.AffineTransform(scale=(1.2, 1), shear=0.2) )
-    t_form.append( tf.AffineTransform(scale=(1.2, 1), shear=-0.2) )
+    t_form.append( tf.AffineTransform(scale=(1.2, 1), shear=0.3) )
+    t_form.append( tf.AffineTransform(scale=(1.2, 1), shear=-0.3) )
 
     t7_form = img * np.random.choice([0, 1], img.shape, p=[0.1, 0.9])
-    t8_form = img * np.random.choice([0, 1], img.shape, p=[0.25, 0.75])
+    # t8_form = img * np.random.choice([0, 1], img.shape, p=[0.25, 0.75])
 
 
     images.append( img )
@@ -45,25 +45,46 @@ def transform_image(img):
 
 
     images.append( t7_form )
-    images.append( t8_form )
+    # images.append( t8_form )
 
     return images
 
 
+# def center_image(img):
+
+#     cm = ndimage.measurements.center_of_mass(img)
+
+#     tform = np.zeros(img.shape)
+
+#     real_y = int(img.shape[0] / 2)
+#     real_x = int(img.shape[1] / 2)
+
+#     actu_y = int(cm[0])
+#     actu_x = int(cm[1])
+
+#     move_y = real_y - actu_y
+#     move_x = real_x - actu_x
+
+#     tf_form = tf.AffineTransform(translation=(-move_x, -move_y))
+
+#     return tf.warp(img, tf_form)
+
+
 def center_image(img):
 
-    cm = ndimage.measurements.center_of_mass(img)
+    height = img.shape[-2]
+    width = img.shape[-1]
 
-    tform = np.zeros(img.shape)
+    col_sum = np.where( np.sum(img, axis=0) > 0 )
+    row_sum = np.where( np.sum(img, axis=1) > 0 )
 
-    real_y = int(img.shape[0] / 2)
-    real_x = int(img.shape[1] / 2)
+    y1, y2 = row_sum[0][0], row_sum[0][-1]
+    x1, x2 = col_sum[0][0], col_sum[0][-1]
 
-    actu_y = int(cm[0])
-    actu_x = int(cm[1])
+    cropped_image = img[y1:y2, x1:x2]
 
-    move_y = real_y - actu_y
-    move_x = real_x - actu_x
+    move_y = int(((y1 + height - y2) / 2) - y1)
+    move_x = int(((x1 + width - x2) / 2) - x1)
 
     tf_form = tf.AffineTransform(translation=(-move_x, -move_y))
 
